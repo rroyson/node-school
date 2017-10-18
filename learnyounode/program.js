b549d45ab562502261ce0c2d1f1076a4d6d65963
rroyson <frontiersons@gmail.com>
2017-10-17 20:16:20 -0400

    finished 4 & 5

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -32,9 +32,40 @@
 ////Exercise 3
 ///////////////
 
+// var fs = require('fs')
+// var buf = fs.readFileSync(process.argv[2]).toString().split('\n').length-1
+// 
+// 
+// 
+// console.log(callback(err, buf))
+
+///////////////
+////Exercise 4
+///////////////
+
 var fs = require('fs')
-var buf = fs.readFileSync(process.argv[2]).toString().split('\n').length-1
+var buf = process.argv[2]
 
-console.log(buf)
+fs.readFile(buf, function (err, data) {
+  var lines = data.toString().split('\n').length - 1
+  if(err){
+    return err
+  } else {
+    console.log(lines)
+  }
+})
 
+
+//Official Solution
+// var fs = require('fs')
+//  var file = process.argv[2]
+// 
+//  fs.readFile(file, function (err, contents) {
+//    if (err) {
+//      return console.log(err)
+//    }
+//    // fs.readFile(file, 'utf8', callback) can also be used
+//    var lines = contents.toString().split('\n').length - 1
+//    console.log(lines)
+//  })
 
